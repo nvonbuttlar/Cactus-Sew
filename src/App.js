f4a7270316eed06904f5e986884c5c0476d520eb
nvonbuttlar <nick.von.buttlar@gmail.com>
2019-02-21 02:21:43 -0800

    changed css on header - position: fixed, z-index: 1 - so that header stays above background + body

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './styles/App.css';
 import './styles/navButton.css';
 import Background from './images/studio.jpg';
-import Header from './components/Header.js'; // this is bringing in header.css
+import Header from './components/Header.js'; // this is bringing in header.css, but this file in particular is not being used.
 import Logo from './images/logo.png';
 import About from './components/About.js';
 import Gallery from './components/Gallery.js';
 import Contact from './components/Contact.js';
 import Footer from './components/Footer.js';
-
+import Transition from './components/Transition.js';
 
 
 class App extends Component {
@@ -28,28 +28,22 @@ class App extends Component {
             />
 
           <div id="headercontainer">
-
             <div id="titlecontainer">
-              <img className="logo" src={Logo} alt="Cactus Logo"/>
-              <h1 className="title"> Cactus Sewing Studio. </h1>
+              <Link to={'/'} className="link"> <img className="logo" src={Logo} alt="Cactus Logo"/> </Link>
+              <Link to={'/'} className="link"> <h1 className="title"> Cactus Sewing Studio. </h1> </Link>
             </div>
 
             <div className="navBar">
-
-              <Link to={'/about'} className="link">  <button className="navBtn"> About </button> </Link>
-              <Link to={'/gallery'} className="link">  <button className="navBtn"> Gallery </button> </Link>
-              <Link to={'/contact'} className="link">  <button className="navBtn"> Contact </button> </Link>
-
+              <Link to={'/about'} className="link"> <button className="navBtn"> About </button> </Link>
+              <Link to={'/gallery'} className="link"> <button className="navBtn"> Gallery </button> </Link>
+              <Link to={'/contact'} className="link"> <button className="navBtn"> Contact </button> </Link>
             </div>
-
           </div>
 
           <div className="body">
-
-            <Route path="/about" component={About}/>
-            <Route path="/gallery" component={Gallery}/>
-            <Route path="/contact" component={Contact}/>
-
+              <Route path="/about" component={About}/>
+              <Route path="/gallery" component={Gallery}/>
+              <Route path="/contact" component={Contact}/>
           </div>
 
           <Footer/>
