60b7fba85490daf5de0501c25e7ed32a232f4eef
nvonbuttlar <nick.von.buttlar@gmail.com>
2019-07-18 14:40:17 -0700

    reimplement typewriter effect

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { useSpring, animated } from "react-spring";
 
 import "../styles/Landing.css";
 
 function Landing() {
-  const fade_transition = useSpring({ opacity: 1, from: { opacity: 0 } });
   return (
-    <animated.div style={fade_transition} className="container">
-      <h1>Welcome to Cactus Sewing Studio.</h1>
-    </animated.div>
+    <div className="container">
+      <h1 className="css-typing">Welcome to Cactus Sewing Studio.</h1>
+    </div>
   );
 }
 
