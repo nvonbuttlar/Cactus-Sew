b3eed1bdf273e3e7f458af4d8398474db1158f94
nvonbuttlar <nick.von.buttlar@gmail.com>
2019-11-08 19:02:50 -0800

    revert old changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,21 +40,6 @@ const Header = () => {
           </NavLink>
         </div>
       </div>
-      <div id="mobile-header">
-        <NavLink to="/" className="link">
-          <img className="cactus" src={Cactus} alt="Cactus Logo" />
-        </NavLink>
-        <button onClick={() => setdropMenu(!dropMenu)}>
-          <FontAwesomeIcon
-            className="menu-icon"
-            color="white"
-            icon={faBars}
-            size="3x"
-          />
-          ˝
-        </button>
-      </div>
-      {dropMenu && <div className="menu-container">Hello</div>}
     </>
   );
 };
